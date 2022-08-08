447d5620f0bbb037267d5a0dc611461cd2275465
supriyahv25 <supriyapriya2575@gmail.com>
2022-08-08 12:35:27 +0530

    new project

---
+++ src/test/java/Basic/test1.java
@@ -0,0 +1,25 @@
+package Basic;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		//driver.findElement(By.linkText("PIM")).click();
+
+	}
+
+}
